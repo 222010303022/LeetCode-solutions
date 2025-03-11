f73043bf2c2b41a48f0db066f5d6a62df835769f
Karan Singha <69747690+222010303022@users.noreply.github.com>
2025-03-11 12:47:24 +0530

    [238] [Time Beats: 87.16%] [Memory Beats: 93.47%] - LeetPush

--- Product-of-Array-Except-Self.py
+++ Product-of-Array-Except-Self.py
@@ -1,15 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        out = [0] * len(nums)
         l_mult = 1
         r_mult = 1
-        n = len(nums)
-        l_arr = [0] * n
-        r_arr = [0] * n
-        for i in range(n) :
-            j = -i-1
-            l_arr[i] = l_mult
-            r_arr[j] = r_mult
+        for i in range(len(nums)) :
+            out[i] = l_mult
             l_mult *= nums[i]
+        for j in range(len(nums)-1,-1,-1) :
+            out[j] *= r_mult
             r_mult *= nums[j]
-        return [l*r for l,r in zip(l_arr,r_arr)]
+        return out
         
