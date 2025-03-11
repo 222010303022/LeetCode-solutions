ac57244bef1ae16250803d0b9a8c32efcd59bacb
Karan Singha <69747690+222010303022@users.noreply.github.com>
2025-03-11 09:37:10 +0530

    [347] [Time Beats: 93.10%] [Memory Beats: 29.65%] - LeetPush

--- Top-K-Frequent-Elements.py
+++ Top-K-Frequent-Elements.py
@@ -1,14 +1,11 @@
+from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hmap = {}
-        freq = [[] for i in range(len(nums)+1)]
-        for i in nums :
-            hmap[i] = 1 + hmap.get(i,0)
-        for n,c in hmap.items() :
-            freq[c].append(n)
-        res = []
-        for i in range(len(freq)-1,0,-1):
-            for n in freq[i] :
-                res.append(n)
-                if len(res) == k :
-                    return res
+        heap = []
+        counter = Counter(nums)
+        for key,val in counter.items() :
+            if len(heap)<k :
+                heapq.heappush(heap,(val,key))
+            else :
+                heapq.heappushpop(heap,(val,key))
+        return [h[1] for h in heap]
